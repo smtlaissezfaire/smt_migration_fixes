4cc53d46919921394a133aa95813613a6ee427d2
Scott Taylor <scott@railsnewbie.com>
2008-12-22 13:42:58 -0500

    * Add version spec

---
+++ spec/smt_migration_fixes/version_spec.rb
@@ -0,0 +1,11 @@
+require File.dirname(__FILE__) + "/../spec_helper"
+
+module SMT
+  describe MigrationFixes do
+    describe "version" do
+      it "should be at 1.0.0" do
+        MigrationFixes::VERSION.should == "1.0.0"
+      end
+    end
+  end
+end
