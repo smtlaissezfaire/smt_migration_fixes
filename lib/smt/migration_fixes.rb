119deb4c221acb71ba5ad7d8dbe622a9d659357a
Scott Taylor <scott@railsnewbie.com>
2008-12-22 13:44:33 -0500

    * Bump the version to 1.1.0

--- lib/smt/migration_fixes.rb
+++ lib/smt/migration_fixes.rb
@@ -3,7 +3,7 @@ module SMT
   module MigrationFixes
     
     MAJOR = 1
-    MINOR = 0
+    MINOR = 1
     TINY  = 0
     
     VERSION = "#{MAJOR}.#{MINOR}.#{TINY}"
--- spec/smt_migration_fixes/version_spec.rb
+++ spec/smt_migration_fixes/version_spec.rb
@@ -3,8 +3,8 @@ require File.dirname(__FILE__) + "/../spec_helper"
 module SMT
   describe MigrationFixes do
     describe "version" do
-      it "should be at 1.0.0" do
-        MigrationFixes::VERSION.should == "1.0.0"
+      it "should be at 1.1.0" do
+        MigrationFixes::VERSION.should == "1.1.0"
       end
     end
   end
